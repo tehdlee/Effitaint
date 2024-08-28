c388a74df9c90834eb081aad157ef6486ad0b227
TEhD <haocheng0609@gmail.com>
2024-08-28 18:48:15 +0800

    del some files

--- java-benchmarks/java-sec-code/src/main/java/org/joychou/RMI/Hello.java
+++
@@ -1,9 +0,0 @@
-package org.joychou.RMI;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-// 定义远程接口
-public interface Hello extends Remote {
-    String sayHello() throws RemoteException;
-}
